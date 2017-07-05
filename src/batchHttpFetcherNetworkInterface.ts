7e7ec1bba4108c9688f39054686559eab2442d26
Evans Hauser <evanshauser@gmail.com>
2017-07-05 10:19:52 -0700

    Removed batching, since it will be inside of apollo-fetch or apollo-batch

--- src/batchHttpFetcherNetworkInterface.ts
+++
@@ -1,6 +0,0 @@
-import BatchHttpFetcher from './batchHttpFetcher';
-import { fetcherToNetworkInterface } from './fetcher-as-promise';
-
-export default function createBatchHttpFetcherNetworkInterface (uri: string) {
-  return fetcherToNetworkInterface(new BatchHttpFetcher({uri}));
-}
