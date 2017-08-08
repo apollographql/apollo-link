1d4bba0cb965acb787ba981f036bde9a38956a6c
Evans Hauser <evanshauser@gmail.com>
2017-08-08 14:40:32 -0700

    bump graphql types versions and export dedup from apollo-link

--- packages/apollo-link/src/index.ts
+++ packages/apollo-link/src/index.ts
@@ -1,5 +1,6 @@
 import ApolloLink from 'apollo-link-core';
 
+import DedupLink from 'apollo-link-dedup';
 import HttpLink from 'apollo-link-http';
 import RetryLink from 'apollo-link-retry';
 import SetContextLink from 'apollo-link-set-context';
@@ -8,6 +9,13 @@ import WebSocketLink from 'apollo-link-ws';
 
 export * from 'apollo-link-core';
 
-export { HttpLink, RetryLink, SetContextLink, PollingLink, WebSocketLink };
+export {
+  HttpLink,
+  RetryLink,
+  SetContextLink,
+  PollingLink,
+  WebSocketLink,
+  DedupLink,
+};
 
 export default ApolloLink;
