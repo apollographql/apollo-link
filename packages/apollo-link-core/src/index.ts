6e3fe035b155c592a96273db39972a47569adf2a
Evans Hauser <evanshauser@gmail.com>
2017-08-09 00:24:03 -0700

    export zen observable typings grom apollo-link-core and import in polingLink

--- packages/apollo-link-core/src/index.ts
+++ packages/apollo-link-core/src/index.ts
@@ -3,6 +3,7 @@ export { makePromise } from './linkUtils';
 export * from './types';
 
 import Observable from 'zen-observable-ts';
+export * from 'zen-observable-ts';
 
 export default ApolloLink;
 export { Observable, ApolloLink, execute };
