bcaee28bbfb652f268f200facca6b46da07a58fa
Tomas Trescak <tomi.trescak@gmail.com>
2017-09-07 21:42:22 +1000

    Update batchHttpLink.ts
    Headers were not set properly when used with setContext link

--- packages/apollo-link-batch-http/src/batchHttpLink.ts
+++ packages/apollo-link-batch-http/src/batchHttpLink.ts
@@ -66,6 +66,7 @@ export default class BatchHttpLink extends ApolloLink {
   }
 
   public request(operation: Operation): Observable<FetchResult> | null {
+    this.headers = (operation.context && operation.context.headers) || {};
     return this.batcher.request(operation);
   }
 }
