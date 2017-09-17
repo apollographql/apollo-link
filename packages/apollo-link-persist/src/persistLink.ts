8f73783c10c2e87c08eeba8f08e36503118d8d8a
Dhaivat Pandya <dhaivatpandya@gmail.com>
2017-09-17 15:12:13 -0400

    hacked in an id field in there

--- packages/apollo-link-persist/src/persistLink.ts
+++ packages/apollo-link-persist/src/persistLink.ts
@@ -8,16 +8,37 @@ import {
 
 import {
   OutputMap,
+  getQueryDocumentKey,
 } from 'persistgraphql';
 
 export default class PersistLink extends ApolloLink {
   private queryMap: OutputMap;
 
+  constructor(
+    queryMap: OutputMap,
+  ) {
+    super();
+
+    this.queryMap = queryMap;
+  }
+    
   request(
     operation?: Operation,
     forward?: NextLink,
   ): Observable<FetchResult> | null {
-    // TODO Implement query lookups
+    const queryDocument = operation.query;
+    const queryKey = getQueryDocumentKey(queryDocument);
+
+    if (!this.queryMap[queryKey]) {
+      // TODO handle the error if the query key is not found
+    }
+
+    return forward({
+      query: null,
+      id: this.queryMap[queryKey],
+      variables: operation.variables,
+      operationName: operation.operationName,
+    } as Operation);
   }
 }
 
