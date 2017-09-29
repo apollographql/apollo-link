d0ef07750dcad7ba5b70ffac3a93b66c53a00304
James Baxley <james@meteor.com>
2017-09-29 17:27:58 -0400

    updated tests

--- packages/apollo-link-http/src/httpLink.ts
+++ packages/apollo-link-http/src/httpLink.ts
@@ -1,9 +1,11 @@
-import { ApolloLink, Operation, FetchResult, Observable } from 'apollo-link';
-
+import { ApolloLink, Observable } from 'apollo-link';
 import { print } from 'graphql/language/printer';
 
-type ResponseError = Error | { response?: Response; parseError?: Error };
-const parseAndCheckResponse = response => {
+import { ApolloFetch } from 'apollo-fetch';
+
+type ResponseError = Error & { response: Response; parseError: Error };
+
+const parseAndCheckResponse = (response: Response) => {
   return response
     .json()
     .then(result => {
@@ -14,9 +16,9 @@ const parseAndCheckResponse = response => {
       return result;
     })
     .catch(e => {
-      const httpError: ResponseError = new Error(
+      const httpError = new Error(
         `Network request failed with status ${response.status} - "${response.statusText}"`,
-      );
+      ) as ResponseError;
       httpError.response = response;
       httpError.parseError = e;
 
@@ -24,39 +26,50 @@ const parseAndCheckResponse = response => {
     });
 };
 
-const checkFetcher = fetcher => {
+const checkFetcher = (fetcher: ApolloFetch | GlobalFetch['fetch']) => {
   if (
-    fetcher.use &&
-    fetcher.useAfter &&
-    fetcher.batchUse &&
-    fetcher.batchUseAfter
+    (fetcher as ApolloFetch).use &&
+    (fetcher as ApolloFetch).useAfter &&
+    (fetcher as ApolloFetch).batchUse &&
+    (fetcher as ApolloFetch).batchUseAfter
   ) {
+    throw new Error(`
+      It looks like you're using apollo-fetch! Apollo Link now uses the native fetch
+      implementation, so apollo-fetch is not needed. If you want to use your existing
+      apollo-fetch middleware, please check this guide to upgrade:
+        https://github.com/apollographql/apollo-link/blob/master/docs/implementation.md
+    `);
+  }
+};
+
+const warnIfNoFetch = fetcher => {
+  if (!fetcher && typeof fetch === 'undefined') {
+    let library: string = 'unfetch';
+    if (typeof window === 'undefined') library = 'node-fetch';
     throw new Error(
-      `It looks like you're using apollo-fetch! Apollo Link now uses the native fetch implementation, so apollo-fetch is not needed. If you want to use your existing apollo-fetch middleware, please check this guide to upgrade: https://github.com/apollographql/apollo-link/blob/master/docs/implementation.md`,
+      `fetch is not found globally and no fetcher passed, to fix pass a fetch for
+      your environment like https://www.npmjs.com/package/${library}.
+
+      For example:
+        import fetch from '${library}';
+        import { createFetchLink } from 'apollo-link-fetch';
+
+        const link = createFetchLink({ uri: '/graphql', fetch: fetch });
+      `,
     );
   }
 };
 
 export interface FetchOptions {
   uri?: string;
-  fetch?: Fetch;
+  fetch?: GlobalFetch['fetch'];
   includeExtensions?: boolean;
 }
-export const createFetchLink = ({
-  uri,
-  fetch: fetcher,
-  includeExtensions,
-}: FetchOptions) => {
-  if (!fetcher && typeof fetch === 'undefined') {
-    let link: string = 'https://www.npmjs.com/package/unfetch';
-    if (typeof window === 'undefined') {
-      link = 'https://www.npmjs.com/package/node-fetch';
-    }
-    throw new Error(
-      `fetch is not found globally and no fetcher passed, to fix pass a fetch via ${link}`,
-    );
-  }
-
+export const createFetchLink = (
+  { uri, fetch: fetcher, includeExtensions }: FetchOptions = {},
+) => {
+  // dev warnings to ensure fetch is present
+  warnIfNoFetch(fetcher);
   if (fetcher) checkFetcher(fetcher);
 
   // use default global fetch is nothing passed in
@@ -69,11 +82,16 @@ export const createFetchLink = ({
         const { headers, credentials, fetcherOptions } = operation.getContext();
         const { operationName, extensions, variables, query } = operation;
 
-        const body = { operationName, variables, query: print(query) };
-        if (includeExtensions) body.extensions = extensions;
+        const body = {
+          operationName,
+          variables,
+          query: print(query),
+        };
+        if (includeExtensions) (body as any).extensions = extensions;
 
+        let serializedBody;
         try {
-          const serializedBody = JSON.stringify(body);
+          serializedBody = JSON.stringify(body);
         } catch (e) {
           throw new Error(
             `Network request failed. Payload is not serializable: ${e.message}`,
@@ -82,6 +100,7 @@ export const createFetchLink = ({
 
         const fetchOptions = {
           method: 'POST',
+          ...fetcherOptions,
           headers: {
             // headers are case insensitive (https://stackoverflow.com/a/5259004)
             accept: '*/*',
@@ -89,19 +108,10 @@ export const createFetchLink = ({
           },
           body: serializedBody,
         };
-        // XXX check for signal API;
 
         if (credentials) fetchOptions.credentials = credentials;
         if (headers)
           fetchOptions.headers = { ...fetchOptions.headers, ...headers };
-
-        /*
-         * XXX
-         *
-         * - support an array of operations (batch)
-         * - support canceling
-         *
-         */
 
         fetcher(uri, fetchOptions)
           .then(parseAndCheckResponse)
@@ -122,7 +132,13 @@ export const createFetchLink = ({
 };
 
 export class FetchLink extends ApolloLink {
+  private fetcher: ApolloLink;
   constructor(opts: FetchOptions) {
-    this.request = createFetchLink(opts);
+    super();
+    this.fetcher = createFetchLink(opts);
+  }
+
+  public request(operation) {
+    return this.fetcher.request(operation);
   }
 }
