b1edbc1c3c94c9afab564940ac233a4fab184249
James Baxley <james@meteor.com>
2017-10-20 13:14:15 -0700

    add new context link with async support

---
+++ packages/apollo-link-context/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change log
+
+### vNEXT
+- Added the operation and any data to the error handler callback
+- changed graphqlErrors to be graphQLErrors to be consistent with Apollo Client
---
+++ packages/apollo-link-context/rollup.config.js
@@ -0,0 +1,17 @@
+export default {
+  entry: 'lib/index.js',
+  dest: 'lib/bundle.umd.js',
+  format: 'umd',
+  sourceMap: true,
+  moduleName: 'errorLink',
+  exports: 'named',
+  onwarn,
+};
+
+function onwarn(message) {
+  const suppressed = ['UNRESOLVED_IMPORT', 'THIS_IS_UNDEFINED'];
+
+  if (!suppressed.find(code => message.code === code)) {
+    return console.warn(message.message);
+  }
+}
