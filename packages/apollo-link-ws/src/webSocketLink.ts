263a233edafa719a0794a5f458f4c321735670f9
Ian MacLeod <ian@nevir.net>
2017-12-02 16:39:00 -0800

    Explicit configuration interface for WebSocketLink

--- packages/apollo-link-ws/src/webSocketLink.ts
+++ packages/apollo-link-ws/src/webSocketLink.ts
@@ -2,16 +2,37 @@ import { ApolloLink, Operation, FetchResult, Observable } from 'apollo-link';
 
 import { SubscriptionClient, ClientOptions } from 'subscriptions-transport-ws';
 
-export type WebSocketParams = {
-  uri: string;
-  options?: ClientOptions;
-  webSocketImpl?: any;
-};
+export namespace WebSocketLink {
+  /**
+   * Configuration to use when constructing the subscription client (subscriptions-transport-ws).
+   */
+  export interface Configuration {
+    /**
+     * The endpoint to connect to.
+     */
+    uri: string;
+
+    /**
+     * Options to pass when constructing the subscription client.
+     */
+    options?: ClientOptions;
+
+    /**
+     * A custom WebSocket implementation to use.
+     */
+    webSocketImpl?: any;
+  }
+}
+
+// For backwards compatibility.
+export import WebSocketParams = WebSocketLink.Configuration;
 
 export class WebSocketLink extends ApolloLink {
   private subscriptionClient: SubscriptionClient;
 
-  constructor(paramsOrClient: WebSocketParams | SubscriptionClient) {
+  constructor(
+    paramsOrClient: WebSocketLink.Configuration | SubscriptionClient,
+  ) {
     super();
 
     if (paramsOrClient instanceof SubscriptionClient) {
