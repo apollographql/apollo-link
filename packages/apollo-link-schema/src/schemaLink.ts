7651b616de505c99cd9db0f543e32062e204e2da
Ian MacLeod <ian@nevir.net>
2017-12-02 17:32:43 -0800

    Expose SchemaLink's options as a named interface

--- packages/apollo-link-schema/src/schemaLink.ts
+++ packages/apollo-link-schema/src/schemaLink.ts
@@ -1,20 +1,31 @@
 import { ApolloLink, Operation, FetchResult, Observable } from 'apollo-link';
 import { execute, GraphQLSchema } from 'graphql';
 
+export namespace SchemaLink {
+  export interface Options {
+    /**
+     * The schema to generate responses from.
+     */
+    schema: GraphQLSchema;
+
+    /**
+     * The root value to use when generating responses.
+     */
+    rootValue?: any;
+
+    /**
+     * A context to provide to resolvers declared within the schema.
+     */
+    context?: any;
+  }
+}
+
 export class SchemaLink extends ApolloLink {
   public schema: GraphQLSchema;
   public rootValue: any;
   public context: any;
 
-  constructor({
-    schema,
-    rootValue,
-    context
-  }: {
-    schema: GraphQLSchema;
-    rootValue?: any;
-    context?: any;
-  }) {
+  constructor({ schema, rootValue, context }: SchemaLink.Options) {
     super();
 
     this.schema = schema;
