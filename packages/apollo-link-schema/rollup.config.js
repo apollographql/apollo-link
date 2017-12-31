9293001349b43720ddfb5acb4d1dfc79ed786172
Evans Hauser <evanshauser@gmail.com>
2017-12-31 12:05:45 -0800

    apollo-link-schema rollup config uses global onwarn

--- packages/apollo-link-schema/rollup.config.js
+++ packages/apollo-link-schema/rollup.config.js
@@ -1,3 +1,5 @@
+import { onwarn } from '../../rollup.config';
+
 export default {
   input: 'lib/schemaLink.js',
   output: {
@@ -9,11 +11,3 @@ export default {
   },
   onwarn,
 };
-
-function onwarn(message) {
-  const suppressed = ['UNRESOLVED_IMPORT', 'THIS_IS_UNDEFINED'];
-
-  if (!suppressed.find(code => message.code === code)) {
-    return console.warn(message.message);
-  }
-}
