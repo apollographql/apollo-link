bb86473e3c9b4ce4b910c9ab65622ea5b7d9c3ed
David Glasser <glasser@davidglasser.net>
2018-02-20 15:11:31 -0800

    apollo-link-http: factor out GET logic into function

--- packages/apollo-link-http/src/httpLink.ts
+++ packages/apollo-link-http/src/httpLink.ts
@@ -7,6 +7,7 @@ import {
   selectHttpOptionsAndBody,
   createSignalIfSupported,
   fallbackHttpConfig,
+  Body,
   HttpOptions,
   UriFunction as _UriFunction,
 } from 'apollo-link-http-common';
@@ -71,60 +72,11 @@ export const createHttpLink = (linkOptions: HttpLink.Options = {}) => {
     if (controller) (options as any).signal = signal;
 
     if (options.method === 'GET') {
-      // Implement the standard HTTP GET serialization, plus 'extensions'. Note
-      // the extra level of JSON serialization!
-      const queryParams = [];
-      const addQueryParam = (key: string, value: string) => {
-        queryParams.push(`${key}=${encodeURIComponent(value)}`);
-      };
-
-      if ('query' in body) {
-        addQueryParam('query', body.query);
+      const { newURI, parseError } = rewriteURIForGET(chosenURI, body);
+      if (parseError) {
+        return fromError(parseError);
       }
-      if (body.operationName) {
-        addQueryParam('operationName', body.operationName);
-      }
-      if (body.variables) {
-        let serializedVariables;
-        try {
-          serializedVariables = serializeFetchParameter(
-            body.variables,
-            'Variables map',
-          );
-        } catch (parseError) {
-          return fromError(parseError);
-        }
-        addQueryParam('variables', serializedVariables);
-      }
-      if (body.extensions) {
-        let serializedExtensions;
-        try {
-          serializedExtensions = serializeFetchParameter(
-            body.extensions,
-            'Extensions map',
-          );
-        } catch (parseError) {
-          return fromError(parseError);
-        }
-        addQueryParam('extensions', serializedExtensions);
-      }
-
-      // Reconstruct the URI with added query params.
-      // XXX This assumes that the URI is well-formed and that it doesn't
-      //     already contain any of these query params. We could instead use the
-      //     URL API and take a polyfill (whatwg-url@6) for older browsers that
-      //     don't support URLSearchParams. Note that some browsers (and
-      //     versions of whatwg-url) support URL but not URLSearchParams!
-      let fragment = '',
-        preFragment = chosenURI;
-      const fragmentStart = chosenURI.indexOf('#');
-      if (fragmentStart !== -1) {
-        fragment = chosenURI.substr(fragmentStart);
-        preFragment = chosenURI.substr(0, fragmentStart);
-      }
-      const queryParamsPrefix = preFragment.indexOf('?') === -1 ? '?' : '&';
-      chosenURI =
-        preFragment + queryParamsPrefix + queryParams.join('&') + fragment;
+      chosenURI = newURI;
     } else {
       try {
         (options as any).body = serializeFetchParameter(body, 'Payload');
@@ -193,6 +145,66 @@ export const createHttpLink = (linkOptions: HttpLink.Options = {}) => {
     });
   });
 };
+
+// For GET operations, returns the given URI rewritten with parameters, or a
+// parse error.
+function rewriteURIForGET(chosenURI: string, body: Body) {
+  // Implement the standard HTTP GET serialization, plus 'extensions'. Note
+  // the extra level of JSON serialization!
+  const queryParams = [];
+  const addQueryParam = (key: string, value: string) => {
+    queryParams.push(`${key}=${encodeURIComponent(value)}`);
+  };
+
+  if ('query' in body) {
+    addQueryParam('query', body.query);
+  }
+  if (body.operationName) {
+    addQueryParam('operationName', body.operationName);
+  }
+  if (body.variables) {
+    let serializedVariables;
+    try {
+      serializedVariables = serializeFetchParameter(
+        body.variables,
+        'Variables map',
+      );
+    } catch (parseError) {
+      return { parseError };
+    }
+    addQueryParam('variables', serializedVariables);
+  }
+  if (body.extensions) {
+    let serializedExtensions;
+    try {
+      serializedExtensions = serializeFetchParameter(
+        body.extensions,
+        'Extensions map',
+      );
+    } catch (parseError) {
+      return { parseError };
+    }
+    addQueryParam('extensions', serializedExtensions);
+  }
+
+  // Reconstruct the URI with added query params.
+  // XXX This assumes that the URI is well-formed and that it doesn't
+  //     already contain any of these query params. We could instead use the
+  //     URL API and take a polyfill (whatwg-url@6) for older browsers that
+  //     don't support URLSearchParams. Note that some browsers (and
+  //     versions of whatwg-url) support URL but not URLSearchParams!
+  let fragment = '',
+    preFragment = chosenURI;
+  const fragmentStart = chosenURI.indexOf('#');
+  if (fragmentStart !== -1) {
+    fragment = chosenURI.substr(fragmentStart);
+    preFragment = chosenURI.substr(0, fragmentStart);
+  }
+  const queryParamsPrefix = preFragment.indexOf('?') === -1 ? '?' : '&';
+  const newURI =
+    preFragment + queryParamsPrefix + queryParams.join('&') + fragment;
+  return { newURI };
+}
 
 export class HttpLink extends ApolloLink {
   public requester: RequestHandler;
