119d8ebc396260fcf428e24e641a1841e54e24b0
Kiran Sonnad <ksonnad@gmail.com>
2018-02-21 11:51:34 -0800

    Separate the test for Promise-returning custom attempt functions

--- packages/apollo-link-retry/src/__tests__/retryLink.ts
+++ packages/apollo-link-retry/src/__tests__/retryLink.ts
@@ -145,7 +145,6 @@ describe('RetryLink', () => {
     const attemptStub = jest.fn();
     attemptStub.mockReturnValueOnce(true);
     attemptStub.mockReturnValueOnce(true);
-    attemptStub.mockReturnValueOnce(Promise.resolve(true));
     attemptStub.mockReturnValueOnce(false);
 
     const retry = new RetryLink({
@@ -162,7 +161,29 @@ describe('RetryLink', () => {
       [1, operation, standardError],
       [2, operation, standardError],
       [3, operation, standardError],
-      [4, operation, standardError],
     ]);
   });
-});
+
+
+  it('supports custom attempt functions that return either Promises or booleans', async () => {
+    const attemptStub = jest.fn();
+    attemptStub.mockReturnValueOnce(true);
+    attemptStub.mockReturnValueOnce(Promise.resolve(true));
+    attemptStub.mockReturnValueOnce(Promise.resolve(false));
+
+    const retry = new RetryLink({
+      delay: { initial: 1 },
+      attempts: attemptStub,
+    });
+    const linkStub = jest.fn(() => new Observable(o => o.error(standardError)));
+    const link = ApolloLink.from([retry, linkStub]);
+    const [{ error }] = await waitFor(execute(link, { query }));
+
+    expect(error).toEqual(standardError);
+    const operation = attemptStub.mock.calls[0][1];
+    expect(attemptStub.mock.calls).toEqual([
+      [1, operation, standardError],
+      [2, operation, standardError],
+      [3, operation, standardError],
+    ]);
+  });
