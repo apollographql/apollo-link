87bfc494e3de9d57b0ea41f2d5d529e29f4edaaa
Evans Hauser <evanshauser@gmail.com>
2018-02-22 11:52:58 -0800

    add zen-observable-ts to externals

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ export const globals = {
   'apollo-link': 'apolloLink.core',
   'apollo-link-batch': 'apolloLink.batch',
   'apollo-utilities': 'apollo.utilities',
+  'zen-observable-ts': 'apolloLink.zenObservable',
 
   //GraphQL
   'graphql/language/printer': 'printer',
