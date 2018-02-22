41b05c603bbfb7c637145231b6e86c5b91749f4e
Evans Hauser <evanshauser@gmail.com>
2018-02-22 15:58:52 -0800

    exporting zen-observable default directly

--- packages/zen-observable-ts/src/zenObservable.ts
+++ packages/zen-observable-ts/src/zenObservable.ts
@@ -1,5 +1,12 @@
 declare function require(name: string);
-const _Observable = require('zen-observable');
+
+const Observable: {
+  new <T>(subscriber: Subscriber<T>): Observable<T>;
+  from<R>(...args: Array<R>): Observable<R>;
+  of<R>(...args: Array<R>): Observable<R>;
+} = require('zen-observable');
+
+export default Observable;
 
 import { ZenObservable } from './types';
 
@@ -9,63 +16,25 @@ export type Observer<T> = ZenObservable.Observer<T>;
 export type Subscriber<T> = ZenObservable.Subscriber<T>;
 export type ObservableLike<T> = ZenObservable.ObservableLike<T>;
 
-export default class Observable<T> extends (_Observable as {
-  new (subscriber: any): any;
-  from(...args): any;
-  of(...args): any;
-}) {
-  public static from<R>(
-    observable: Observable<R> | ZenObservable.ObservableLike<R> | ArrayLike<R>,
-  ): Observable<R> {
-    return super.from(observable);
-  }
-
-  public static of<R>(...items: R[]): Observable<R> {
-    return super.of(...items);
-  }
-
-  constructor(subscriber: ZenObservable.Subscriber<T>) {
-    super(subscriber) as any;
-  }
-
-  public subscribe(
+export interface Observable<T> {
+  subscribe(
     observerOrNext: ((value: T) => void) | ZenObservable.Observer<T>,
     error?: (error: any) => void,
     complete?: () => void,
-  ): ZenObservable.Subscription {
-    return super.subscribe(observerOrNext, error, complete);
-  }
+  ): ZenObservable.Subscription;
 
-  public forEach(fn: (value: T) => void): Promise<void> {
-    return super.forEach(fn);
-  }
+  forEach(fn: (value: T) => void): Promise<void>;
 
-  public map<R>(fn: (value: T) => R): Observable<R> {
-    return super.map(fn);
-  }
+  map<R>(fn: (value: T) => R): Observable<R>;
 
-  public filter(fn: (value: T) => boolean): Observable<T> {
-    return super.filter(fn);
-  }
+  filter(fn: (value: T) => boolean): Observable<T>;
 
-  public reduce<R = T>(
+  reduce<R = T>(
     fn: (previousValue: R | T, currentValue: T) => R | T,
     initialValue?: R | T,
-  ): Observable<R | T> {
-    return super.reduce(fn, initialValue);
-  }
+  ): Observable<R | T>;
 
-  public concat(...sources: Array<Observable<T>>) {
-    return super.concat(...sources);
-  }
+  concat(...sources: Array<Observable<T>>);
 
-  public flatMap<R>(
-    fn: (value: T) => ZenObservable.ObservableLike<R>,
-  ): Observable<R> {
-    if (this.observable.flatMap) {
-      return super.flatMap(fn);
-    } else {
-      console.error('this zen-observable does does not support flatMap');
-    }
-  }
+  flatMap<R>(fn: (value: T) => ZenObservable.ObservableLike<R>): Observable<R>;
 }
