3f5261c889f5a6f742e89f2893685cdec2b1a60f
jdecroock <decroockjovi@gmail.com>
2019-01-20 17:11:10 +0100

    feat: update rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import node from 'rollup-plugin-node-resolve';
 
 export const globals = {
   // Apollo
@@ -15,20 +16,62 @@ export const globals = {
   'zen-observable': 'Observable',
 };
 
-export default name => ({
-  input: 'lib/index.js',
-  output: {
-    file: 'lib/bundle.umd.js',
-    format: 'umd',
-    name: `apolloLink.${name}`,
-    globals,
-    sourcemap: true,
-    exports: 'named',
-  },
-  external: Object.keys(globals),
-  onwarn,
-  plugins: [sourcemaps()],
-});
+export default name => [
+  {
+    input: 'lib/index.js',
+    output: {
+      file: 'lib/bundle.umd.js',
+      format: 'umd',
+      name: `apolloLink.${name}`,
+      globals,
+      sourcemap: true,
+      exports: 'named',
+    },
+    external: Object.keys(globals),
+    onwarn,
+    plugins: [
+      node({
+        module: true,
+        only: ['tslib']
+      }),
+      sourcemaps()
+    ],
+  }, {
+    input: 'lib/index.js',
+    output: {
+      file: 'lib/bundle.cjs.js',
+      format: 'cjs',
+      globals,
+      sourcemap: true,
+    },
+    external: Object.keys(globals),
+    onwarn,
+    plugins: [
+      node({
+        module: true,
+        only: ['tslib']
+      }),
+      sourcemaps()
+    ],
+  }, {
+    input: 'lib/index.js',
+    output: {
+      file: 'lib/bundle.esm.js',
+      format: 'esm',
+      globals,
+      sourcemap: true,
+    },
+    external: Object.keys(globals),
+    onwarn,
+    plugins: [
+      node({
+        module: true,
+        only: ['tslib']
+      }),
+      sourcemaps()
+    ],
+  }
+];
 
 export function onwarn(message) {
   const suppressed = ['UNRESOLVED_IMPORT', 'THIS_IS_UNDEFINED'];
