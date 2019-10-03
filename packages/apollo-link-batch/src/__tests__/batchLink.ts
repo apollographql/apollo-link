7255594537b3273dd5769b94dfcd99a73752a881
hwillson <hugh@octonary.com>
2019-10-03 14:09:55 -0400

    Replicate test utilities from `@apollo/client` to reduce exports
    These are only be used by this test, so we'll create a copy of
    them here to stop re-exporting them from `@apollo/client`.

--- packages/apollo-link-batch/src/__tests__/batchLink.ts
+++ packages/apollo-link-batch/src/__tests__/batchLink.ts
@@ -3,7 +3,6 @@ import {
   execute,
   Operation,
   FetchResult,
-  createOperation,
   GraphQLRequest,
   Observable,
 } from '@apollo/client';
@@ -22,6 +21,45 @@ interface MockedResponse {
   result?: FetchResult;
   error?: Error;
   delay?: number;
+}
+
+function getKey(operation: GraphQLRequest) {
+  // XXX We're assuming here that query and variables will be serialized in
+  // the same order, which might not always be true.
+  const { query, variables, operationName } = operation;
+  return JSON.stringify([operationName, query, variables]);
+}
+
+export function createOperation(
+  starting: any,
+  operation: GraphQLRequest,
+): Operation {
+  let context = { ...starting };
+  const setContext = (next: any) => {
+    if (typeof next === 'function') {
+      context = { ...context, ...next(context) };
+    } else {
+      context = { ...context, ...next };
+    }
+  };
+  const getContext = () => ({ ...context });
+
+  Object.defineProperty(operation, 'setContext', {
+    enumerable: false,
+    value: setContext,
+  });
+
+  Object.defineProperty(operation, 'getContext', {
+    enumerable: false,
+    value: getContext,
+  });
+
+  Object.defineProperty(operation, 'toKey', {
+    enumerable: false,
+    value: () => getKey(operation),
+  });
+
+  return operation as Operation;
 }
 
 const terminatingCheck = (done, body) => {
